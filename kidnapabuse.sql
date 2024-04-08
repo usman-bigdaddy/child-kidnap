683a06c1a1f11093f1f9ae6afcdc4e48c1863045
usman-bigdaddy <bigdaddy2030@gmail.com>
2024-04-08 07:35:16 +0100

    added stuff

--- kidnapabuse.sql
+++ kidnapabuse.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 26, 2024 at 04:23 PM
--- Server version: 10.4.25-MariaDB
--- PHP Version: 7.4.30
+-- Generation Time: Apr 08, 2024 at 08:34 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -36,18 +36,19 @@ CREATE TABLE `tb_cases` (
   `category` enum('kidnap','abuse') DEFAULT NULL,
   `case_time` time DEFAULT current_timestamp(),
   `case_state` varchar(25) DEFAULT NULL,
-  `case_address` varchar(225) DEFAULT NULL
+  `case_address` varchar(225) DEFAULT NULL,
+  `case_status` enum('Solved','Pending','Closed') DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tb_cases`
 --
 
-INSERT INTO `tb_cases` (`case_id`, `case_date`, `case_title`, `case_description`, `cover_image`, `category`, `case_time`, `case_state`, `case_address`) VALUES
-(1264, '2024-03-26', 'End sars', 'burnt cars and shops', 'end-sars.jpeg', 'abuse', '15:08:35', 'FCT', 'Wuse market in front of fidelity bank'),
-(1984, '2024-03-26', 'End sars lagos', 'burnt cars and shops lagos', 'end-sars.jpeg', 'kidnap', '15:15:16', 'Lagos', 'MainlAnd Bridge'),
-(1988, '2024-03-26', 'Chibok Girls', 'the school girls were abducted at night', 'chibok-girls.jpg', 'kidnap', '15:15:16', 'Kaduna', 'Behind malali school'),
-(4514, '2024-03-21', 'Mararaba', 'took some people', 'end-sars.jpeg', 'kidnap', '12:15:16', 'FCT', 'Front of that nyanay bridge');
+INSERT INTO `tb_cases` (`case_id`, `case_date`, `case_title`, `case_description`, `cover_image`, `category`, `case_time`, `case_state`, `case_address`, `case_status`) VALUES
+(1264, '2024-03-26', 'End sars', 'burnt cars and shops', 'end-sars.jpeg', 'abuse', '15:08:35', 'FCT', 'Wuse market in front of fidelity bank', 'Solved'),
+(1984, '2024-03-26', 'End sars lagos', 'burnt cars and shops lagos', 'end-sars.jpeg', 'kidnap', '15:15:16', 'Lagos', 'MainlAnd Bridge', 'Pending'),
+(1988, '2024-03-26', 'Chibok Girls', 'the school girls were abducted at night', 'chibok-girls.jpg', 'kidnap', '15:15:16', 'Kaduna', 'Behind malali school', 'Solved'),
+(4514, '2024-03-21', 'Mararaba', 'took some people', 'end-sars.jpeg', 'kidnap', '12:15:16', 'FCT', 'Front of that nyanay bridge', 'Pending');
 
 --
 -- Indexes for dumped tables
